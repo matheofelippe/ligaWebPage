c5f8c04916071af56aebab8dd53bc4c0512a8639
Felippe Matheo <felippe.marquesin@facens.br>
2024-09-27 19:01:54 -0300

    new updates on login page responsivity

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../components/button/button.component";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-
+  
 }
