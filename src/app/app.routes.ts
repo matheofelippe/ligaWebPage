1aeae06dfaf34ce3424a6a5ded3041682e839953
Felippe Matheo <felippe.marquesin@facens.br>
2024-10-16 10:50:57 -0300

    Finalizacao da login page e new component register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginPageComponent},
+    {path: 'register', component: RegisterPageComponent},
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: '**', redirectTo: 'login', pathMatch: 'full'}
+];
