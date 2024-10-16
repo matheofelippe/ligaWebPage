f593728174a6430bd5e20d84219133623cf062a5
Felippe Matheo <felippe.marquesin@facens.br>
2024-10-16 18:52:27 -0300

    register page progress

--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../components/button/button.component";
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
